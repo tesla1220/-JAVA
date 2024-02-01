2835a7f76b5a1355802bdf107740dfe514f7fac9
jina <iwbyl1220@gmail.com>
2024-02-01 18:09:30 +0900

    [수업 내용 정리] 2024-02-01

---
+++ src/main/java/com/ohgiraffers/section03/constant/practice.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section03.constant;
+
+public class practice {
+
+    public static void main(String[] args) {
+
+        int inum1 = 20;
+        int inum2 = 30;
+
+
+
+        System.out.println("더하기 결과 = " + (inum1 + inum2) );
+
+        System.out.println("빼기 결과 = " + (inum1 - inum2));
+
+        System.out.println( "곱하기 결과 = " + (inum1 * inum2));
+
+        System.out.println( "나누기한 몫 = " + (inum1 / inum2));
+
+        System.out.println( " 나누기한 나머지" + (inum1 % inum2));
+
+        double wide = 12.5;
+        double height = 36.4;
+
+        System.out.println( "면적 = " + (wide * height));
+        System.out.println( "둘레 = " + ((wide * 2) + (height*2)) );
+
+
+    }
+}

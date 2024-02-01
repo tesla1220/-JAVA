301d2e32b620d6d682acdba7a23924359db8a075
jina <iwbyl1220@gmail.com>
2024-02-01 17:28:48 +0900

    [수업 내용 정리] 2024-02-01

---
+++ src/main/java/com/ohgiraffers/section02/variable/practice.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section02.variable;
+
+public class practice {
+    public static void main(String[] args) {
+        int inum;
+        inum = 3;
+        inum = 4;
+        System.out.println(inum + inum);
+
+        String str = "안녕";
+        double dnum;
+        dnum = 30.0;
+        System.out.println(str + (dnum + dnum));
+
+        System.out.println(true + "");
+        System.out.println('J' + "ava");
+//        System.out.println(true + null);
+
+    }
+}
